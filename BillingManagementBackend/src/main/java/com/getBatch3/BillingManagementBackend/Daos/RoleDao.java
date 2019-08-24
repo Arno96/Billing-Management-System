4fd5b682e5781fa312955fa31510e232a706237f
sourcecode369 <sourcecode369@gmail.com>
2019-08-24 19:34:17 +0530

    add all files

--- BillingManagementBackend/src/main/java/com/getBatch3/BillingManagementBackend/Daos/RoleDao.java
+++
@@ -1,9 +0,0 @@
-package com.getBatch3.BillingManagementBackend.Daos;
-
-import java.util.List;
-
-import com.getBatch3.BillingManagementBackend.models.Role;
-
-public interface RoleDao {
-	public List<Role> getAllRole();
-}
